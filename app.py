116818c1dd5144a505c029953a7749785fccc09a
saurab1102 <58526210+saurab1102@users.noreply.github.com>
2024-02-26 17:57:13 +0000

    login bareshell

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
+from database import *
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return render_template("home.html")
 
+@app.route("/login", methods = ['post'])
+def login():
+  data = request.form
+  return data
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080, debug=True)
